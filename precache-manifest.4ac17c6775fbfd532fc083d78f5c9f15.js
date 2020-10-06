bd07d7c939978a9c92d548990ec7769daaa109a0
anacobic <anacobic@gmail.com>
2020-10-06 21:17:01 +0200

    Updates

---
+++ precache-manifest.4ac17c6775fbfd532fc083d78f5c9f15.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3df94acedd31143b7e40",
+    "url": "/react_math_test_app/static/js/runtime~main.faf9b2c8.js"
+  },
+  {
+    "revision": "f458bf29073a4bc8f70a",
+    "url": "/react_math_test_app/static/js/main.dae14b83.chunk.js"
+  },
+  {
+    "revision": "d9562a260558f2dd98e7",
+    "url": "/react_math_test_app/static/js/2.4b4f80f2.chunk.js"
+  },
+  {
+    "revision": "f458bf29073a4bc8f70a",
+    "url": "/react_math_test_app/static/css/main.fb9d2dac.chunk.css"
+  },
+  {
+    "revision": "b11bcc552c99960331089d313c057375",
+    "url": "/react_math_test_app/index.html"
+  }
+];
